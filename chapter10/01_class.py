689708198e1de08d19ded879e218fd15ac1481dd
Aniket Khangar <aniketkhangar459@gmail.com>
2026-06-18 10:15:46 +0530

    Chapter 8 and 9

---
+++ chapter10/01_class.py
@@ -0,0 +1,14 @@
+class Employee:
+    language="Py"
+    salary=12345
+
+harry=Employee()
+harry.name="Harry"
+print(harry.name,harry.language,harry.salary)
+
+
+aniket=Employee()
+aniket.name="Aniket"
+print(aniket.name,aniket.language,aniket.salary)
+
+# here 'name' is instance/object attribute and language and salary are class attributes as they directly belong to the class
---
+++ chapter10/02_instance_vs_class_attribute.py
@@ -0,0 +1,12 @@
+class Employee:
+    language="Py"
+    salary=12345
+
+harry=Employee()
+harry.name="Harry"
+print(harry.name,harry.language,harry.salary)
+
+
+aniket=Employee()
+aniket.name="Aniket"
+print(aniket.name,aniket.language,aniket.salary)
